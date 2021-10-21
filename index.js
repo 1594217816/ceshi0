3fce4001e231796dd11ceb16bf7d2c1f4ff8081d
ShenKun <1594217816@qq.com>
2021-10-21 20:37:18 +0800

    第二次提交

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-10-21 20:31:14
- * @LastEditTime: 2021-10-21 20:33:42
+ * @LastEditTime: 2021-10-21 20:37:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \git\index.js
  * sadsadsadsad
  */
-
-let i = 1;
+dsasadsad
+let i = 0;
+sdasdsadsa
 
 
 let sdasdsadsa;
