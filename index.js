de618e9ae840ea111a875feb8312d880ab9df0ba
ShenKun <1594217816@qq.com>
2021-10-21 20:31:56 +0800

    第一次提交

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-21 20:31:14
+ * @LastEditTime: 2021-10-21 20:31:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \git\index.js
+ */
+
+let i = 1;
